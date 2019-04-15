2433c9da6d18672a321b4424a7a159e111c6252f
Miguel Angel <devmiguelangel@outlook.com>
2019-04-15 16:35:19 -0400

    circle create

--- cride/circles/views/circles.py
+++ cride/circles/views/circles.py
@@ -8,7 +8,7 @@ from rest_framework.permissions import IsAuthenticated
 from cride.circles.serializers import CircleModelSerializer
 
 # Models
-from cride.circles.models import Circle
+from cride.circles.models import Circle, Membership
 
 
 class CircleViewSet(viewsets.ModelViewSet):
@@ -19,9 +19,28 @@ class CircleViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthenticated, )
 
     def get_queryset(self):
+        """ Restrict list to public only. """
+
         queryset = Circle.objects.all()
 
         if self.action == 'list':
             return queryset.filter(is_public=True)
 
         return queryset
+
+    def perform_create(self, serializer):
+        """ Assign circle admin. """
+
+        circle = serializer.save()
+        user = self.request.user
+        profile = user.profile
+
+        Membership.objects.create(
+            user=user,
+            profile=profile,
+            circle=circle,
+            is_admin=True,
+            remaining_invitations=10
+        )
+
+        return super().perform_create(serializer)
