e407633d31001802cb0144ea3616566915bccd99
Miguel Angel <devmiguelangel@outlook.com>
2019-04-17 16:43:52 -0400

    admin actions

--- cride/circles/admin.py
+++ cride/circles/admin.py
@@ -21,3 +21,19 @@ class CircleAdmin(admin.ModelAdmin):
     )
     search_fields = ('slug_name', 'name')
     list_filter = ('is_public', 'is_verified', 'is_limited')
+
+    actions = ['make_verified', 'make_unverified']
+
+    def make_verified(self, request, queryset):
+        """ Make circles verified. """
+
+        queryset.update(is_verified=True)
+
+    make_verified.short_description = 'Make selected circles verified.'
+
+    def make_unverified(self, request, queryset):
+        """ Make circles unverified. """
+
+        queryset.update(is_verified=False)
+
+    make_unverified.short_description = 'Make selected circles unverified.'
