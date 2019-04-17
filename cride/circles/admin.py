8593eb76291ff8af46a93b5625304766f95b2347
Miguel Angel <devmiguelangel@outlook.com>
2019-04-17 17:29:51 -0400

    admin csv response

--- cride/circles/admin.py
+++ cride/circles/admin.py
@@ -2,9 +2,16 @@
 
 # Django
 from django.contrib import admin
+from django.http import HttpResponse
 
 # Models
 from .models import Circle
+from cride.rides.models import Ride
+
+# Utils
+import csv
+import datetime
+from django.utils import timezone
 
 
 @admin.register(Circle)
@@ -22,7 +29,7 @@ class CircleAdmin(admin.ModelAdmin):
     search_fields = ('slug_name', 'name')
     list_filter = ('is_public', 'is_verified', 'is_limited')
 
-    actions = ['make_verified', 'make_unverified']
+    actions = ['make_verified', 'make_unverified', 'download_todays_rides']
 
     def make_verified(self, request, queryset):
         """ Make circles verified. """
@@ -37,3 +44,42 @@ class CircleAdmin(admin.ModelAdmin):
         queryset.update(is_verified=False)
 
     make_unverified.short_description = 'Make selected circles unverified.'
+
+    def download_todays_rides(self, request, queryset):
+        """ Return today's rides. """
+
+        now = timezone.now()
+        start = datetime.datetime(now.year, now.month, now.day, 0, 0, 0)
+        end = start + datetime.timedelta(days=1)
+        rides = Ride.objects.filter(offered_in__in=queryset.values_list('id'),
+                                    departure_date__gte=start,
+                                    departure_date__lte=end).order_by('departure_location')
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="rides.csv"'
+
+        writer = csv.writer(response)
+        writer.writerow([
+            'id',
+            'passengers',
+            'departure_location',
+            'departure_date',
+            'arrival_location',
+            'arrival_date',
+            'rating'
+        ])
+
+        for ride in rides:
+            writer.writerow([
+                ride.pk,
+                ride.passengers.count(),
+                ride.departure_location,
+                str(ride.departure_date),
+                ride.arrival_location,
+                str(ride.arrival_date),
+                ride.rating
+            ])
+
+        return response
+
+    download_todays_rides.short_description = 'Download todays rides'
